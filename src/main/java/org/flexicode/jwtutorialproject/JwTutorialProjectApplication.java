24c166cf40f95638f109da57e705be8b05f9acb8
= <=>
2023-03-20 11:53:51 +0100

    Successfully implemented the Tutor and Student classes

---
+++ src/main/java/org/flexicode/jwtutorialproject/JwTutorialProjectApplication.java
@@ -0,0 +1,13 @@
+package org.flexicode.jwtutorialproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwTutorialProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwTutorialProjectApplication.class, args);
+    }
+
+}
